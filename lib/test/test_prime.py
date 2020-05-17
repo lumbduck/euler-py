a07c07471db9709d1db2383ba6b353b391511cd1
Richard Shafer <lumbduck@gmail.com>
2020-05-16 21:45:26 -0700

    Test file cleanup

--- lib/test/test_prime.py
+++ lib/test/test_prime.py
@@ -1,14 +1,15 @@
 from functools import update_wrapper
-import prime as prime
+import prime
 import pytest
 
 
-# Pytest fixture to clear the prime cache during test setup
+# Pytest fixtures
 @pytest.fixture()
 def clear_prime_cache():
+    """Clear module-level prime caching in prime.py before executing a test."""
     print("\nClearing prime cache")
     prime.clear_prime_cache()
-    yield None
+    yield
 
 
 # Clean up lru_cache wrappers so that introspection works during test reporting
@@ -16,6 +17,10 @@ update_wrapper(prime.lru_cache, prime.prime_factors)
 update_wrapper(prime.lru_cache, prime.prime_factors_precomputed)
 update_wrapper(prime.lru_cache, prime.sum_divisors)
 
+
+########################
+# Test data
+########################
 
 # First 100 primes, used for several tests
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
@@ -27,6 +32,10 @@ reduce_by_factor_expected = [
     ((12, 2), (3, 2))
 ]
 
+
+########################
+# Tests
+########################
 
 def test_reduce_by_factor():
     for pair in reduce_by_factor_expected:
