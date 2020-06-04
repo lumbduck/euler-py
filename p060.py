4b457c6b5f443337e408ca7136ae1c8eb3c62c93
Richard Shafer <lumbduck@gmail.com>
2020-06-03 23:11:20 -0700

    p60-p62
    p60 is extremely slow and should be rewritten

---
+++ p060.py
@@ -0,0 +1,86 @@
+"""
+Prime Pair Sets
+
+The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and concatenating them in any order the result will always be prime. For example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
+
+Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+"""
+from collections import defaultdict
+from itertools import chain
+
+from prime import is_prime, primes, sieve_primes
+from common import elapsed, split_timer
+
+target_set_size = 5
+
+# For each prime, p, keep a set of primes, q with p > q, for which (p, q) satisfy the pair_primality test.
+prime_pairs = defaultdict(set)
+
+
+def populate_prime_pairs(p, q):
+    """Return True if concatening m and n in both orders produces two primes."""
+    if p in prime_pairs and q in prime_pairs[p]:
+        return True
+
+    elif is_prime(int(str(p) + str(q))) and is_prime(int(str(q) + str(p))):
+        prime_pairs[p].add(q)
+        return True
+
+
+def get_connected_set(p_pairs, n):
+    """Return a set of n primes from the set p_pairs for which all pairs satisfy pair primality with each other, if such a set exists."""
+    if len(p_pairs) >= n:
+        if n == 1:
+            # Done
+            return p_pairs
+
+        # Starting from large prime in p_pairs, start checking for pair primes all the way down
+        #   stopping only if there are not enough primes left to reach n,
+        #   or if we have found n pairwise connected primes
+        p_pairs = sorted(p_pairs, reverse=True)
+        for i in range(len(p_pairs) - n + 1):
+            q = p_pairs[i]
+            if q in prime_pairs:
+                # Recurse
+                q_set = get_connected_set(prime_pairs[q].intersection(p_pairs[i + 1:]), n - 1)
+
+                if q_set:
+                    q_set.add(q)
+                    return q_set
+
+            elif q == 3 and n == 2:
+                # Backup base case, since 3 will never be added to prime_pairs dict
+                return {3}
+
+    return set()
+
+
+def solve_by_pairing(target_size=target_set_size):
+    # This algorithm will populate prime_pairs with each prime (key) pointing to a set of all
+    #   smaller primes that form a valid prime pair with it.
+    # Each of these is then checked by get_connected_set to see if there are enough pairwise
+    #   connections to complete the problem.
+
+    # NOTE: prime index starts from 3 to avoid primes 2 and 5, which cannot be part of any prime pair.
+    # 3 is chained into the inner loop, but not the outer loop because no valid primes are less than 3.
+    log_step = 1000
+    next_step = 0
+
+    for p in primes(step=10000, start_index=3):
+        for q in chain(reversed(sieve_primes(max_prime=p)[3:-1]), [3]):
+            populate_prime_pairs(p, q)
+
+        if p > next_step:
+            split_timer()
+            print(f"Reached p > {next_step} -- {p}: {prime_pairs.get(p)}")
+            next_step += log_step
+
+        if p in prime_pairs:
+            p_set = get_connected_set(prime_pairs[p], target_size - 1)
+            if p_set:
+                p_set.add(p)
+                return sorted(p_set)[:target_size]
+
+
+print(solve_by_pairing(5))
+elapsed()  # Around 20 minutes!
---
+++ p062.py
@@ -0,0 +1,92 @@
+"""
+Cubic Permutations
+
+The cube, 41063625 (345^3), can be permuted to produce two other cubes: 56623104 (384^3) and 66430125 (405^3). In fact, 41063625 is the smallest cube which has exactly three permutations of its digits which are also cube.
+
+Find the smallest cube for which exactly five permutations of its digits are cube.
+"""
+from collections import defaultdict
+from itertools import count
+from math import ceil, factorial
+
+from numb import same_digits
+
+from common import elapsed, split_timer
+
+target = 5
+
+
+def get_cubes(start, stop):
+    return set(k**3 for k in range(start, stop))
+
+
+def get_start_order(target=target):
+    return next(filter(lambda x: x[1] >= target, ((i, factorial(i)) for i in count(1))))[0]
+
+
+def sorted_digits(n):
+    return tuple(sorted(str(n)))
+
+
+#  ~6s
+def solve_w_set_manipulation(target=target):
+    # Cubes will be checked in chunks by limiting the domain of f(x) = x^(1/3) to a given order of magnitude.
+    # This allows precomputation of all cubes in a given range of permutations.
+    order = get_start_order()
+    start = ceil(10**((order - 1) / 3))
+
+    for i in count(order):
+        stop = ceil(10**(i / 3))
+        cubes = get_cubes(start, stop)
+        cube_count = len(cubes)
+
+        split_timer()
+        print(f"Checking {len(cubes)} cubes between {start:,}^3 = {start**3:,} and {stop:,}^3 = {stop**3:,}")
+        while cubes:
+            # Remove a random element
+            c = cubes.pop()
+            c_digits = str(c)
+
+            # And then remove permutations of that element
+            c_perms = [p for p in cubes if same_digits(p, c_digits)]
+            cubes.difference_update(c_perms)
+
+            # If the set length has changed by exactly our target amount then we're done
+            if cube_count - len(cubes) == target:
+                return min(c_perms)
+            else:
+                cube_count = len(cubes)
+
+        start = stop
+
+
+#  ~0.0218s
+def solve_w_counter(target=target):
+    """Much faster than solve_w_set_manipulation, this simply keys the cubes off their sorted digits so they can be counted quickly."""
+    # Cubes will be checked in chunks by limiting the domain of f(x) = x^(1/3) to a given order of magnitude.
+    # This allows precomputation of all cubes in a given range of permutations.
+    order = get_start_order()
+    start = ceil(10**((order - 1) / 3))
+
+    for i in count(order):
+        stop = ceil(10**(i / 3))
+
+        cube_gen = (k**3 for k in range(start, stop))
+        cubes = defaultdict(list)
+
+        # Store cubes in a dict keyed by the sorted digits
+        for c in cube_gen:
+            cubes[sorted_digits(c)].append(c)
+
+        split_timer()
+        print(f"Checking {len(cubes)} cubes between {start:,}^3 = {start**3:,} and {stop:,}^3 = {stop**3:,}")
+
+        for c, v in cubes.items():
+            if len(v) == target:
+                return min(v)
+
+        start = stop
+
+
+print(f"{solve_w_counter()} is the smallest cube which has exactly {target} cubes among the permutations of its digits")
+elapsed()
